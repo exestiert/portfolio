58ace785d1f8a69e662416e5c9d2e3164d6abd83
exestiert <steffenlw94@gmail.com>
2023-06-13 10:46:46 +0200

    AboutMe text geändert und Funktion zur errechnung des Alters eingefügt.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,6 +1,14 @@
 import styles from "../styles/AboutMe.module.css";
 import Navigation from "./Navigation";
 const AboutUs = () => {
+  function getAge(oDate) {
+    let diff_ms = Date.now() - oDate.getTime();
+    let age_ms = new Date(diff_ms);
+    let age_years = age_ms.getUTCFullYear() - 1970;
+    return age_years < 0 ? NaN : age_years;
+  }
+  
+  let oBirthDay = new Date(1997, 1, 10);
   return (
     <div className={styles.background}>
       <Navigation />
@@ -9,18 +17,12 @@ const AboutUs = () => {
         <h1>Steffen Lemke</h1>
         <h3> Frontend Entwickler</h3>
         <p>
-          Hi, ich bin Steffen Lemke (26), ein angehender Web Entwickler. Zurzeit
-          mache ich eine Weiterbildung als Fullstack Webentwickler bei Digital
-          Career Institute. Da ich zuvor eine Ausbildung zum
+          Hi, ich bin Steffen Lemke {getAge(oBirthDay)}, ein Web Entwickler auf der Suche nach einer neuen Herausforderung. Da ich zuvor eine Ausbildung zum
           Gestaltungstechnischen Assistenten absolvierte, habe ich mich für die
           Weiterbildung als Webentwickler entschieden um die gestalterischen
           Fähigkeiten mit der Programierung zu kombinieren. <br />
           <br />
-          Durch meine Weiterbildung bei DIgitial Career Institute beherrsche ich
-          verschiedene Programmiersprachen wie HTML, CSS, JavaScript und React.
-          Ich konnte diese Fähigkeiten bei verschiedenen Projekten erfolgreich
-          eingesetzt, wo ich maßgeblich an der Entwicklung des Frontend
-          beteiligt war und die Umsetzung der Designs gewährleistet habe. <br />
+          Durch meine Weiterbildung bei Digitial Career Institute beherrsche ich verschiedene Programmiersprachen wie HTML, CSS, JavaScript und das dazugehörige React Framework. Ich konnte diese Fähigkeiten im Final Projekt erfolgreich einsetzten, wo ich maßgeblich an der Entwicklung des Frontend beteiligt war. Das Final Projekt war eine Gruppenarbeit aus 4 Person und beinhaltet ein Wiki zu Dungeons & Dragons. Der User hat die Möglichkeit einen Account anzulegen, Profilbild und Passwort zu aktualisieren, durch Input Felder  Informationen aus unserer Datenbank zu filtern und Inhalte als Favoriten im Dashboard zu speichern.  <br />
           <br />
           Ich bin ein kommunikativer, hilfsbereiter und zuverlässiger Mensch und
           bin immer bereit, meine Fähigkeiten zu erweitern und mich an neuen
